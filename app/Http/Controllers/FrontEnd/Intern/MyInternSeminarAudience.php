f6e57efb35dd5909908645e29a6ab4c149d587b5
dhindakiflia <63203788+dhindakiflia@users.noreply.github.com>
2020-06-07 19:58:38 +0700

    ni create audience

--- app/Http/Controllers/FrontEnd/Intern/MyInternSeminarAudience.php
+++ app/Http/Controllers/FrontEnd/Intern/MyInternSeminarAudience.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\InternshipAudience;
+use App\Models\User;
 
 class MyInternSeminarAudience extends Controller
 {
@@ -26,15 +27,23 @@ class MyInternSeminarAudience extends Controller
      */
     public function create($id)
     {   
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+
         $datas = DB::table('internships')->where('id',$id)->get();
+        $internaudience = DB::table('internship_audiences')->get();
         $audiences = DB::table('students')
         ->join('internship_audiences', 'internship_audiences.student_id',
         '=', 'students.id')
         ->where('internship_audiences.internship_id',$id)
         ->orderBy('internship_audiences.created_at','desc')
         ->get();
+
         $students = DB::table('students')->get();
+
         return view('klp02.create_audience',compact('datas','students','audiences'));
+        
+        
     }
 
     /**
@@ -46,12 +55,27 @@ class MyInternSeminarAudience extends Controller
     public function store(Request $request)
     {
         
-        $posts = new InternshipAudience;
-        $posts->internship_id = $request->input('internship_id');
-        $posts->student_id = $request->input('student_id');
-        $posts->save();
+        $internaudience = DB::table('internship_audiences')->get();
+        $masuk=true;
+        foreach ($internaudience as $row) {
+            if($row->internship_id == $request->input('internship_id') && 
+            $row->student_id == $request->input('student_id')){
+                
+                $masuk = false;
+                return back()->with('error','Data Sudah Ada!');
+            }
+        }
+
+        if($masuk==true){
+            $posts = new InternshipAudience;
+            $posts->internship_id = $request->input('internship_id');
+            $posts->student_id = $request->input('student_id');
+            $posts->save();
+            return back()->with('success','Berhasil Menyimpan Data!');
+        }
         
-        return back();
+
+        
         
         
     }
@@ -96,9 +120,12 @@ class MyInternSeminarAudience extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id,$student_id)
     {
-        DB::table('internship_audience')->where('internship_id',$id)->delete();
-        return back();
+        
+        DB::table('internship_audiences')->where('internship_id',$id)
+        ->where('student_id', $student_id)->delete();
+        
+        return back()->with('delete', 'Data Berhasil Dihapus!');
     }
 }
--- resources/views/klp02/create_audience.blade.php
+++ resources/views/klp02/create_audience.blade.php
@@ -12,12 +12,13 @@
 <div class="card">
 
     <div class="card-body">
-        @foreach ($datas as $row)
+        
         <form action="./" method="POST">
             @csrf
             <div class="form-group">
-                {{-- <label for="">ID INTERNSHIP</label> --}}
-                <input type="text" value="{{$row->id}}" name="internship_id" hidden></>
+                @foreach ($datas as $row)
+                    {{-- <label for="">ID INTERNSHIP</label> --}}
+                    <input type="text" value="{{$row->id}}" name="internship_id" hidden></>
                 @endforeach
             </div>
             
@@ -25,42 +26,54 @@
             <div class="input-group">
                 
                 <select name="student_id" class="custom-select" id="inputGroupSelect04">
-                     @foreach ($students as $student)
-                    <option value="{{$student->id}}">{{$student->name}}</option>
+                    @foreach ($students as $student)
+                        <option value="{{$student->id}}">{{$student->name}}</option>
                     @endforeach
                 </select>
+
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary" type="submit">Tambah Peserta</button>
                 </div>
             </div>
                 
-            </form> 
-       
+        </form> 
 
 
-                    <table class="table table-outline table-hover">
-                        <br>
-                        <h5>List Peserta Yang Sudah Ditambahkan</h5>
-                        <thead class="thead-light">
-                            <tr>
-                            <th scope="col">NIM</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Aksi</th>
-                            </tr>
-                        </thead>
+        <table class="table table-outline table-hover">
+            <br>
+            {{-- <h5>List Peserta Yang Sudah Ditambahkan</h5> --}}
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">NIM</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
 
-                        <tbody>
+            <tbody>
 
-                            @foreach ($audiences as $row)
-                                <tr>
-                                    <td>{{$row->nim}}</td>
-                                    <td>{{$row->name}}</td>
-                                    <form action="MyInternSeminarAudience.php/destroy"></form>
-                                    <td><a href="././{{$row->id}}" class="btn btn-block btn-primary">Delete</a></td> 
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table> 
-                </div>
-    </div> 
+                @foreach ($audiences as $row)
+                    <tr>
+                        <td>{{$row->nim}}</td>
+                        <td>{{$row->name}}</td>
+                        
+                        <td>
+                            <form method="POST" action="./{{$row->student_id}}" 
+                                onsubmit="return confirm('Yakin akan menghapus data?')">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-danger" value="Delete">
+                                </div>
+                            </form>
+                        </td>
+                        
+                    </tr>
+                @endforeach
+            </tbody>
+        </table> 
+
+    </div>
+</div> 
 @endsection
